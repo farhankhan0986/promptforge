8c7113c4fffbe831f0943e0f63b538c02ae52f5f
Farhan Abid <farhankhan080304@gmail.com>
2026-03-05 19:59:14 +0530

    added cors

--- server/server.js
+++ server/server.js
@@ -10,7 +10,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://promptforge-blush.vercel.app/'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
